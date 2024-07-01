41164db409ac3a3b7b0df1105913855e6be5d05c
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2024-07-01 14:48:37 +0000

    feat: insere ipynb para facilitar a anahlise de dados.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 from extract import extract
 from transform import transform_data
-if sys.argv[1]=='extract':
+'''if sys.argv[1]=='extract':
     extract()
 if sys.argv[1]=='transform':
-    transform_data()
+    transform_data()'''
+transform_data()
--- transform/transform.py
+++ transform/transform.py
@@ -6,4 +6,5 @@ from transform.utils import load_files
 def transform()->None:
     # desenvolver aqui a rotina principal de transformação
     df_sih: pd.DataFrame = load_files()
+    df_sih
 
