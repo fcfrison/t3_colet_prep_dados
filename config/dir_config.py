46af3e4745bdb1d49ec1fa60091d2a1d09675187
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2024-07-02 19:17:07 +0000

    Cria tabela time_table.

--- config/dir_config.py
+++ config/dir_config.py
@@ -2,3 +2,5 @@ import os
 from pathlib import Path
 ROOT_DIR : str = Path(os.getcwd())
 DATA_DIR : str = ROOT_DIR/"data"
+SIH_DIR : str = DATA_DIR/"sih"
+IBGE_DATA : str = DATA_DIR/"tabelas_ibge"
--- extract/extract.py
+++ extract/extract.py
@@ -2,9 +2,9 @@ from typing import List
 from pysus.ftp.databases.sih import SIH
 from pysus.ftp import File, Database
 from utils.utils import create_folder
-from config.dir_config import DATA_DIR
+from config.dir_config import SIH_DIR
 def extract()->None:
-    create_folder(path=DATA_DIR)
+    create_folder(path=SIH_DIR)
     sih : Database = SIH().load()
     sih_list_files : List[File] = sih.get_files("RD", uf="RS", year=2015)
-    sih.download(files=sih_list_files, local_dir=DATA_DIR)
+    sih.download(files=sih_list_files, local_dir=SIH_DIR)
