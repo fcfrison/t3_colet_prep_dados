2d2191fa8246d92b6db7541b1251305a9ce3a82d
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2024-07-04 21:48:09 +0000

    data: insere mais dados.

--- extract/extract.py
+++ extract/extract.py
@@ -3,8 +3,9 @@ from pysus.ftp.databases.sih import SIH
 from pysus.ftp import File, Database
 from utils.utils import create_folder
 from config.dir_config import SIH_DIR
-def extract()->None:
+def extract(ano_inicial:int, ano_final:int)->None:
     create_folder(path=SIH_DIR)
     sih : Database = SIH().load()
-    sih_list_files : List[File] = sih.get_files("RD", uf="RS", year=2015)
-    sih.download(files=sih_list_files, local_dir=SIH_DIR)
+    for ano in range(ano_inicial,ano_final+1):
+        sih_list_files : List[File] = sih.get_files("RD", uf="RS", year=ano)
+        sih.download(files=sih_list_files, local_dir=SIH_DIR)
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 from extract import extract
 from transform import transform_data
 if sys.argv[1]=='extract':
-    extract()
+    ano_inicial : int = int(sys.argv[2])
+    ano_final : int = int(sys.argv[3])
+    extract(ano_inicial=ano_inicial,ano_final=ano_final)
 if sys.argv[1]=='transform':
-    transform_data()
+    transform_data() 
