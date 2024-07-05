07396f63c8c659fc7ce196e64d266d09a3c41650
Felipe Frison <63929778+fcfrison@users.noreply.github.com>
2024-07-05 14:43:08 +0000

    refactor: remove duplicadas de paciente_table.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
-#from extract import extract
+from extract import extract
 from transform import transform_data
-'''if sys.argv[1]=='extract':
+if sys.argv[1]=='extract':
     ano_inicial : int = int(sys.argv[2])
     ano_final : int = int(sys.argv[3])
     #extract(ano_inicial=ano_inicial,ano_final=ano_final)
 if sys.argv[1]=='transform':
-    transform_data() '''
-transform_data()
+    transform_data()
