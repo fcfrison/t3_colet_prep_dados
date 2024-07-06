9a62cb13f93950bc899fccc26960b1eaca96adeb
fcfrison <fcfrison@gmail.com>
2024-07-06 15:39:29 -0300

    refactor: permite extracao de dados de uma uf especifica.

--- extract/extract.py
+++ extract/extract.py
@@ -3,9 +3,9 @@ from pysus.ftp.databases.sih import SIH
 from pysus.ftp import File, Database
 from utils.utils import create_folder
 from config.dir_config import SIH_DIR
-def extract(ano_inicial:int, ano_final:int)->None:
+def extract(ano_inicial:int, ano_final:int, uf:str)->None:
     create_folder(path=SIH_DIR)
     sih : Database = SIH().load()
     for ano in range(ano_inicial,ano_final+1):
-        sih_list_files : List[File] = sih.get_files("RD", uf="RS", year=ano)
+        sih_list_files : List[File] = sih.get_files("RD", uf=uf, year=ano)
         sih.download(files=sih_list_files, local_dir=SIH_DIR)
