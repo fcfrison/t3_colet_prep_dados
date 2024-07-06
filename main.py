76b34e29c21a95543927f294131647cc2114421d
fcfrison <fcfrison@gmail.com>
2024-07-06 15:42:11 -0300

    data: altera dashboard.

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import sys
 from extract import extract
 from transform import transform_data
 if sys.argv[1]=='extract':
-    ano_inicial : int = int(sys.argv[2])
-    ano_final : int = int(sys.argv[3])
-    extract(ano_inicial=ano_inicial,ano_final=ano_final)
+    uf : str = sys.argv[2]
+    ano_inicial : int = int(sys.argv[3])
+    ano_final : int = int(sys.argv[4])
+    extract(ano_inicial=ano_inicial,ano_final=ano_final,uf=uf)
 if sys.argv[1]=='transform':
     transform_data()
